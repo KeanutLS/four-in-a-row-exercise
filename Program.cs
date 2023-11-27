c6e78dca7ade94a71a6b23d2fdd559354f4b37f7
Keanu Van Acker <114578445+KeanutLS@users.noreply.github.com>
2023-11-27 13:32:04 +0100

    Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
-﻿namespace PRB99.ASN.FourInOneRow
+using System.Numerics;
+using System.IO;
+using System.Security.AccessControl;
+
+namespace PRB99.ASN.FourInOneRow
 {
     internal class Program
     {
-        private int rows = 6;
-        private int columns = 7;
-        private char[,] board;
-        private string[] playerNames = new string[2];
-        private char[] playerSymbols = { 'R', 'G' };
-        private char currentPlayer = 'R'; // Initializing the current player with red because red always starts.
-        private int moves = 0;
+        int rows = 6; // We use 6 rows and 7 columns because that's the standard for Connect Four.
+        int columns = 7;
+        char[,] board; //not initialized yet but will be initialized in the main function
+        string[] playerNames = new string[2]; // We use a string array to store the names of the players.
+        char[] playerSymbols = { 'O', '0' }; // We use a char array to store the symbols of the players.
+        char currentPlayerSymbol = 'O'; // Initializing the first player with red because red always starts.
+        int moves = 0;
+
         static void Main(string[] args)
         {
             Program game1 = new Program();
 
+            game1.board = new char[game1.rows, game1.columns + 1]; //We initialize the board first, we also add 1 to the columns because we want to add a column number to the board.
+            game1.GetNames(); // Get the names of the players.
             game1.RunGame();
         }
 
@@ -28,18 +35,104 @@
         // Draw the game board.
         public void DrawBoard()
         {
-            Console.WriteLine("    1    2    3    4    5    6    7"); // Indicate which column has which number.
+            Console.WriteLine("   1   2   3   4   5   6   7"); // Indicate which column has which number.
 
             for (int i = 0; i < rows; i++) // Iterate through the rows to place symbols in each column.
             {
-                Console.WriteLine("  -----------------------------------");
+                Console.WriteLine("  -----------------------------");
                 for (int j = 0; j < columns; j++) // Iterate through the columns to place symbols. Use a double loop to fill rows first and then columns.
                 {
-                    Console.Write($"  | {board[i, j]} ");
+                    if (board[i, j] == 'O')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red; // Set text color to red for Player 1's symbol.
+                    }
+                    else if (board[i, j] == '0')
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow; // Set text color to yellow for Player 2's symbol.
+                    }
+
+                    Console.Write($" | {board[i, j]} ");
+
+                    Console.ResetColor(); // Reset text color to default.
                 }
-                Console.WriteLine(" |"); // Close with a | to format the board correctly.
+                Console.WriteLine("|"); // Close with a | to format the board correctly.
             }
         }
+
+        // Process player input.
+        public void PlayerInput(string input)
+        {
+            int column = IsInputValid(input); // Check if the input is valid and store the column in the variable column.
+
+            if (column >= 1 && column <= 7) // Check if the input is valid by checking if the column is not -1.
+            {
+                if (!IsColumnFull(column)) // Check if the column is full.
+                {
+                    PlaceCoins(column); // Place the coin in the column.
+
+                    // Check for win after the player makes a move.
+                    if (CheckIfWin())
+                    {
+                        Console.Clear(); // Clear the console to display the final state of the board.
+                        DrawBoard(); // Draw the final state of the board.
+
+                        Console.WriteLine($"Congratulations {GetCurrentPlayer()}! You won!"); // If the current player has won, congratulate them.
+                        Console.WriteLine();
+                        Console.WriteLine("Would you like to play again? (y/n)");
+
+                        string playAgainInput = Console.ReadLine().ToLower();
+
+                        if (playAgainInput == "y")
+                        {
+                            Console.WriteLine("Do you want to continue using the same names? (y/n)");
+                            string input2 = Console.ReadLine().ToLower();
+                            if (input2 == "n")
+                            {
+                                GetNames(); // Get new names for the players.
+                            }
+
+                            board = new char[rows, columns + 1]; // Reset the board for a new game.
+                            moves = 0; // Reset the move count.
+                        }
+                        else if (playAgainInput == "n")
+                        {
+                            Console.WriteLine("Thanks for playing!");
+                            return;
+                        }
+                    }
+                    SwitchPlayer(); // Switch to the other player.
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid column (1-7). Try again.");
+                }
+            }
+        }
+
+        // Check if the input for columns is valid. Note: We check input for columns, not rows, because in Connect Four, you can only place a coin at the top, not on the sides.
+        public int IsInputValid(string userInput)
+        {
+            try // We use a try-catch block to check if the input is an integer.
+            {
+                int parsedColumn = int.Parse(userInput); // Parse the input to an integer.
+
+                if (parsedColumn >= 1 && parsedColumn <= columns) // Check if the input is between 1 and 7.
+                {
+                    return parsedColumn; // Return the column if the input is valid.
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid column (1-7). Try again."); // If the input is not valid, tell the user to try again.
+                    return -1; // Return -1 if the input is not valid.
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid column (1-7). Try again."); // If the input is not an integer, tell the user to try again.
+                return -1; // Return -1 if the input is not valid.
+            }
+        } // This returns true if the input is an integer and is between 1 and 7.
 
         // Place coins in the specified column.
         public void PlaceCoins(int column)
@@ -48,84 +141,25 @@
             {
                 if (board[i, column - 1] == '\0') // Check if the spot is empty; if yes, place the coin of the current player. '\0' represents an empty spot in a char array.
                 {
-                    board[i, column - 1] = currentPlayer; // Use -1 because the array starts at 0 while the user starts at 1.
+                    board[i, column - 1] = currentPlayerSymbol; // Use -1 because the array starts at 0 while the user starts at 1.
                     moves++; // Increment the move count to check later if the board is full.
                     break;
                 }
-                else
-                {
-                    Console.WriteLine("You cannot make a move here, try again");
-                }
             }
-        }
-
-        // Process player input.
-        public void PlayerInput(string input)
-        {
-            int column;
-
-            if (IsValidInput(input, out column)) // First, check if the player provides valid input using the IsValidInput function.
-            {
-                if (!IsColumnFull(column)) // Then, check if the columns are full using the IsColumnFull function.
-                {
-                    PlaceCoins(column); // If both conditions are true, place the coin of the current player.
-                }
-                else
-                {
-                    Console.WriteLine("Invalid move. Column is full. Try again."); // If the column is full, display an error message.
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input. Please enter a valid column (1-7). Try again."); // If the input is not valid, display an error message.
-            }
-        }
-
-        // Check if the input for columns is valid. Note: We check input for columns, not rows, because in Connect Four, you can only place a coin at the top, not on the sides.
-        public bool IsValidInput(string input, out int column)
-        {
-            column = 0;
-            return int.TryParse(input, out column) && column >= 1 && column <= columns; // This returns true if the input is an integer and is between 1 and 7.
-        }   // We use an out int because we want to store the column in the function and use it in the PlaceCoins function.
-
-        // Check for a draw in the game.
-        public bool CheckForDraw()
-        {
-            bool isDraw = false;
-            if (moves > 42) // The maximum number of moves a Connect Four game can have with this board is 42 (calculated by 7*6).
-            {
-                isDraw = true;
-            }
-            if (isDraw == true)
-            {
-                Console.WriteLine("The board is full, it's a draw");
-            }
-            return isDraw;
-        }
-
-        // Check if the specified row in the column is full.
-        public bool CheckIfRowFull(int column)
-        {
-            for (int i = rows - 1; i >= 0; i--)
-            {
-                if (board[i, column] == '\0')
-                {
-                    return false;
-                }
-            }
-            return true;
         }
 
         // Check if the specified column is full.
         public bool IsColumnFull(int column)
         {
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < rows; i++) // Iterate through the rows to check if the column is full.
             {
-                if (board[i, column] == '\0')
+                if (board[i, column - 1] == '\0') // Check if the spot is empty; if yes, return false.
                 {
                     return false;
                 }
             }
+            Console.WriteLine("Column is full. Please choose another column.");
+
             return true;
         }
 
@@ -133,7 +167,7 @@
         public string GetCurrentPlayer()
         {
             string currentPlayerName = "";   // Create an empty string to store the name of the current player.
-            if (currentPlayer == playerSymbols[0]) // Check which player is currently playing based on the symbol, and store the player's name in the string.
+            if (currentPlayerSymbol == playerSymbols[0]) // Check which player is currently playing based on the symbol, and store the player's name in the string.
             {
                 currentPlayerName = playerNames[0];
             }
@@ -143,21 +177,105 @@
             }
             return currentPlayerName;
         }
+        public void SwitchPlayer()
+        {
+            int currentPlayerIndex = Array.IndexOf(playerSymbols, currentPlayerSymbol); // Get the index of the current player in the playerSymbols array.
 
+            // Toggle to the other player
+            currentPlayerSymbol = playerSymbols[(currentPlayerIndex + 1) % playerSymbols.Length]; //we use the modulo operator to switch between the players
+            if (moves == 0)
+            {
+                // If it's the first move in a new game, set the currentPlayerSymbol to the symbol of the first player.
+                currentPlayerSymbol = playerSymbols[0];
+            }
+        }
+        public bool CheckIfWin()
+        {
+            bool hasWon = false;
+
+            //Check for horizontal win
+            for (int row = 0; row < rows; row++) // Iterate through the rows first to look from bottom to top.
+            {
+                for (int col = 0; col <= columns - 4; col++) // Iterate through the columns to look from left to right.
+                {
+                    if (board[row, col] == currentPlayerSymbol &&     // Check if the current player has four coins in a row horizontally.
+                        board[row, col + 1] == currentPlayerSymbol && // We check if the next three columns have the same symbol as the current player.
+                        board[row, col + 2] == currentPlayerSymbol &&
+                        board[row, col + 3] == currentPlayerSymbol)
+                    {
+                        hasWon = true;
+                    }
+                }
+            }
+
+            //Check for vertical win
+
+            for (int row = 0; row <= rows - 4; row++)
+            {
+                for (int col = 0; col < columns; col++)
+                {
+                    if (board[row, col] == currentPlayerSymbol && // Check if the current player has four coins in a row veritically.
+                        board[row + 1, col] == currentPlayerSymbol && // We check if the next three rows have the same symbol as the current player.
+                        board[row + 2, col] == currentPlayerSymbol &&
+                        board[row + 3, col] == currentPlayerSymbol)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            //Check for diagonal win
+            for (int row = 0; row <= rows - 4; row++)
+            {
+                // Check for diagonal win from left to right.
+                for (int col = 0; col <= columns - 4; col++)
+                {
+                    if (board[row, col] == currentPlayerSymbol && // Check if the current player has four coins in a row diagonally.
+                        board[row + 1, col + 1] == currentPlayerSymbol && // We check if the next three rows and columns have the same symbol as the current player.
+                        board[row + 2, col + 2] == currentPlayerSymbol &&
+                        board[row + 3, col + 3] == currentPlayerSymbol)
+
+                    {
+                        hasWon = true;
+                    }
+                    // Check for diagonal win from right to left.
+                    if (board[row + 3, col] == currentPlayerSymbol && //Bottom-left to top-right, 3 steps.
+                          board[row + 2, col + 1] == currentPlayerSymbol && // Up-right, 2 up, 1 right.
+                          board[row + 1, col + 2] == currentPlayerSymbol && //Up-right, 1 up, 2 right.
+                          board[row, col + 3] == currentPlayerSymbol) //Top - right, 3 steps right.
+                    {
+                        hasWon = true;
+                    }
+                }
+            }
+            return hasWon;
+        }
+        //Check for a draw.
+        public bool CheckForDraw()
+        {
+            bool isDraw = false;
+            if (moves >= rows * columns) // Check if the number of moves equals the total number of cells on the board.
+            {
+                isDraw = true;
+                Console.WriteLine("The board is full, it's a draw");
+            }
+            return isDraw;
+        }
         // Run the Connect Four game.
         public void RunGame()
         {
-            GetNames();
             do
             {
-                DrawBoard();
+                Console.Clear(); // Clear the console to make the game look better.
+                DrawBoard(); // Draw the board first to show the player the current state of the board.
+
                 Console.WriteLine($"It's {GetCurrentPlayer()}'s turn. Enter a column number (1-7) to place your coin.");
-                PlayerInput();
-                CheckIfRowFull();
-                IsColumnFull();
-            } while (!CheckForDraw());
+                PlayerInput(Console.ReadLine()); // Get the player's input and process it.
+               
+                string winnerText = $"time: {DateTime.Now}, {playerNames[0]} vs {playerNames[1]}/ winner is {GetCurrentPlayer()}! Congrats!"; // Create a string to store the winner's name and the time they won.
+                File.AppendAllText("log.txt", winnerText + Environment.NewLine); // Append the winner's name and the time they won to the log file.
+            } while (true);
         }
+
     }
 }
-
-// Sources: ChatGPT => Used to review my code, check if I was on the right track, understand how to use functions, and clean up my code.
